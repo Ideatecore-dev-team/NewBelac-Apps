c69ff2fadc44ec6e603f79874cf1be1f1e9f5740
DevQlin <kaitodeluxe678@gmail.com>
2025-07-06 02:16:39 +0700

    FEAT: collection detail-card

--- src/app/walletInventory/layout.tsx
+++ src/app/walletInventory/layout.tsx
@@ -2,7 +2,7 @@ import DetailCard from "../ui/collections/detail-card"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="mt-25">
+        <div className="mt-10">
             <DetailCard />
             <div>{children}</div>
         </div>
