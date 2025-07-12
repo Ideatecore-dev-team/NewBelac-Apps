76de5446603123d5b41bf8955b79a43b6db0a56c
WeissCurry <Maulanasykari@gmail.com>
2025-07-13 03:18:19 +0700

    STYLE : outline sama outline-1 artinya sama euy

--- src/app/ui/collections/collection-card.tsx
+++ src/app/ui/collections/collection-card.tsx
@@ -4,7 +4,7 @@ export default function CollectionCard({ data }: { data: any }) {
     return (
         <div id="collection-card-container" className="w-full bg-Color-Grey-2 rounded-md outline outline-offset-[-1px] outline-[#2C2C2C] inline-flex flex-col justify-start items-start">
             <div id="collection-card-wrapper-top" className="self-stretch h-48 relative bg-gradient-to-b from-white/0 to-black/20 rounded-md outline outline-offset-[-1px] outline-[#2C2C2C] overflow-hidden">
-                <div className="absolute right-2 top-2 hover:bg-[#2C2C2C] disabled:bg-[#333] disabled:cursor-not-allowed disabled:opacity-50 transition-colors p-3 bg-transparent rounded-md outline outline-1 outline-offset-[-1px] outline-[#2C2C2C]">
+                <div className="absolute right-2 top-2 hover:bg-[#2C2C2C] disabled:bg-[#333] disabled:cursor-not-allowed disabled:opacity-50 transition-colors p-3 bg-transparent rounded-md outline outline-offset-[-1px] outline-[#2C2C2C]">
                     <Image
                         priority
                         height={20}
