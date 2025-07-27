64acfd78fa74ae7baeb7ba00cca59ea8f3992eca
DevQlin <kaitodeluxe678@gmail.com>
2025-07-27 16:03:04 +0700

    FEAT: filter holder

--- src/app/collections/holder/page.tsx
+++ src/app/collections/holder/page.tsx
@@ -1,9 +1,11 @@
 // pages/index.tsx
 'use client'; // Ini akan menjadi Client Component karena menggunakan state
 
-import React from 'react';
+import React, { useState } from 'react';
 import Table, { TableColumn, TableRowData } from '@/app/ui/table/baseTable';
 import Image from 'next/image';
+import { BaseTextbox, LegendInputBox } from '@/app/ui/inputbox';
+import { TextButton } from '@/app/ui/button';
 
 interface WalletRow extends TableRowData {
     id: string; // Misal ID dompet atau sebagian dari alamat
@@ -14,7 +16,7 @@ interface WalletRow extends TableRowData {
     moreItems: number; // Jumlah item lainnya
 }
 
-const HomePage: React.FC = () => {
+const HolderPage: React.FC = () => {
     const walletData: WalletRow[] = [
         {
             id: 'wallet-1',
@@ -105,11 +107,79 @@ const HomePage: React.FC = () => {
         },
     ];
 
+    const [selectedSortFilter, setSelectedSortFilter] = useState('');
+    const [quantityOwnedFilter, setQuantityOwnedFilter] = useState({
+        min: 0,
+        max: 0
+    })
+    const sortOption = [
+        { value: 'mostOwned', label: 'Most Owned' },
+        { value: 'highestPrice', label: 'Highest Price' },
+        { value: 'lowestPrice', label: 'Lowest Price' },
+    ];
+
+    const handleChangeAddItemModal = (prop: any) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        setQuantityOwnedFilter({ ...quantityOwnedFilter, [prop]: event.target.value })
+    }
+
     return (
-        <div className="min-h-screen text-gray-300">
-            <Table data={walletData} columns={walletColumns} />
+        <div id='holder-page-container' className="min-h-screen text-gray-300 flex gap-3.5">
+            <div id='filter-wrapper' className='min-w-[295px]'>
+
+                <div className="filter-card flex w-full px-4 pt-4 pb-6 flex-col items-start gap-[15px] rounded-md border border-[#2C2C2C] bg-[#1A1A1A]">
+                    <h1 className="self-stretch justify-start text-white text-base font-semibold font-['D-DIN-PRO'] uppercase leading-none tracking-wide">Filters</h1>
+
+                    <LegendInputBox
+                        value={selectedSortFilter}
+                        onChangeSelect={(e) => setSelectedSortFilter(e.target.value)}
+                        options={sortOption}
+                        placeholder="Select Option"
+                        size="M"
+                        color="Netral"
+
+                        // tambah ini bedanya
+                        legendText="Sort    "
+                        typeBox='select'
+                    />
+
+                    <div className='w-full'>
+                        <label className={`text-base font-semibold'`}>
+                            Quantity Owned
+                        </label>
+                        <div className='mt-3 inline-flex justify-center items-center gap-3'>
+                            <BaseTextbox
+                                value={quantityOwnedFilter.min === 0 ? '' : quantityOwnedFilter.min}
+                                onChangeInput={handleChangeAddItemModal('min')}
+                                placeholder="Min"
+                                size="M"
+                                type='number'
+                            />
+                            To
+                            <BaseTextbox
+                                value={quantityOwnedFilter.max === 0 ? '' : quantityOwnedFilter.max}
+                                onChangeInput={handleChangeAddItemModal('max')}
+                                placeholder="Max"
+                                size="M"
+                                type='number'
+                            />
+                        </div>
+                    </div>
+
+                    <div className='w-full'>
+                        <TextButton
+                            onClick={() => alert('apply filter')}
+                            label="Apply"
+                            size="M"
+                            fullWidth
+                        />
+                    </div>
+                </div>
+            </div>
+            <div id='table-wrapper' className='w-full'>
+                <Table data={walletData} columns={walletColumns} />
+            </div>
         </div>
     );
 };
 
-export default HomePage;
+export default HolderPage;
