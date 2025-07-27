7d1684d0f02ea0c606fc0eda1c999ae9f2a8f0e6
DevQlin <kaitodeluxe678@gmail.com>
2025-07-27 14:16:41 +0700

    FEAT: blank page with connect in /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 'use client'
+import { useAuth } from '@/app/contexts/AuthContext';
+import BlankPage from "./ui/page/blankPage"
 
+export default function page() {
+  const { connectors, connect } = useAuth();
 
-function App() {
+  const metaMaskConnector = connectors.find(
+    (connector) => connector.name === "MetaMask"
+  );
+
   return (
-    <>
-      <div>
-        test
-      </div>
-    </>
+    <div className="relative w-full h-full">
+      <BlankPage
+        title="welcome to project: realmark"
+        subtitle="Start authenticating your physical product"
+        buttonLabel={metaMaskConnector ? 'CONNECT WALLET' : 'METAMASK NOT FOUND'}
+        onClick={() => metaMaskConnector && connect({ connector: metaMaskConnector })}
+        disabled={!metaMaskConnector}
+      />
+    </div>
   )
 }
-
-export default App
