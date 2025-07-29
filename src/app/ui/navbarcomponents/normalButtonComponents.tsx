968d647387afd259f6d3b34056a07fac1c99374a
WeissCurry <Maulanasykari@gmail.com>
2025-07-29 21:41:32 +0700

    CHORE : Xellar kit integrated to our style

--- src/app/ui/navbarcomponents/normalButtonComponents.tsx
+++ src/app/ui/navbarcomponents/normalButtonComponents.tsx
@@ -2,17 +2,31 @@
 
 import { useAuth } from '@/app/contexts/AuthContext';
 import { ConnectButton } from '@xellar/kit';
-import Link from 'next/link'; 
 
-
-export default function NormalButtonComponents() {
-    const { connectors, connect } = useAuth();
+export default function CustomButtonComponent() {
 
     return (
-        // <div className="flex h-[48px] py-[12px] px-[16px] items-center gap-[16px] rounded-[6px] border border-[#2C2C2C] bg-[#1C1C1C] hover:bg-[#2C2C2C] disabled:bg-[#333] disabled:cursor-not-allowed disabled:opacity-50 transition-colors">
-        <div>
+        <ConnectButton.Custom>
+            {({ isConnected, openConnectModal}) => {
+                if (isConnected) {
+                    return (
+                        <h1>
+                            CONNECTED
+                        </h1>
+                    );
+                }
 
-                <ConnectButton />
-        </div>
-    )
+                return (
+                    <button
+                        onClick={openConnectModal}
+                        className="flex h-[48px] py-[12px] px-[16px] items-center gap-[16px] rounded-[6px] border border-[#2C2C2C] bg-[#1C1C1C] hover:bg-[#2C2C2C] disabled:bg-[#333] disabled:cursor-not-allowed disabled:opacity-50 transition-colors"
+                    >
+                        <h1 className="text-white text-[16px] font-semibold leading-4 tracking-[0.5px] font-['D-DIN-PRO']">
+                            CONNECT
+                        </h1>
+                    </button>
+                );
+            }}
+        </ConnectButton.Custom>
+    );
 }
