9ea02784a47359f42864fc3d7987fc225e9de47d
DevQlin <kaitodeluxe678@gmail.com>
2025-07-31 23:56:27 +0700

    FEAT: get collection

--- src/app/walletInventory/collections/page.tsx
+++ src/app/walletInventory/collections/page.tsx
@@ -2,11 +2,14 @@
 import Image from "next/image"
 import CollectionCard from "@/app/ui/collections/collection-card";
 import MiniModal from "@/app/ui/modal/miniModal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IconTextButton } from "@/app/ui/button";
 import { LegendInputBox } from "@/app/ui/inputbox";
 import { redirect, RedirectType, usePathname } from "next/navigation";
+import { useAccount, useReadContract, useReadContracts } from 'wagmi';
 import BlankPage from "@/app/ui/page/blankPage";
+import { COLLECTION_MANAGER_ABI } from "@/constants/COLLECTION_MANAGER_ABI";
+import { COLLECTION_MANAGER_ADDRESS } from "@/constants";
 
 export default function Collections() {
     const [modalEditCollectionIsOpen, setModalEditCollection] = useState<boolean>(false);
@@ -17,14 +20,43 @@ export default function Collections() {
         collectionSymbol: "",
         collectionCategory: "",
     })
+    // const [collectionsData, setCollectionsData] = useState([
+    //     { label: 'Nike Realmark', price: 10.1, items: 68, ava: "https://placehold.co/48x48", image: "https://placehold.co/300x200.png" },
+    //     { label: 'Nike Realmark', price: 10.1, items: 68, ava: "https://placehold.co/48x48", image: "https://placehold.co/300x200" },
+    //     { label: 'Nike Realmark', price: 10.1, items: 68, ava: "/images/nike.png", image: "https://placehold.co/300x200.png" },
+    //     { label: 'Nike Realmark', price: 10.1, items: 68, ava: "https://placehold.co/48x48", image: "/images/nike.png" },
+    //     { label: 'Nike Realmark', price: 10.1, items: 68, ava: "/images/nike.png", image: "/images/nike.png" },
+    // ])
 
-    const collectionsData = [
-        { label: 'Nike Realmark', price: 10.1, items: 68, ava: "https://placehold.co/48x48", image: "https://placehold.co/300x200.png" },
-        { label: 'Nike Realmark', price: 10.1, items: 68, ava: "https://placehold.co/48x48", image: "https://placehold.co/300x200" },
-        { label: 'Nike Realmark', price: 10.1, items: 68, ava: "/images/nike.png", image: "https://placehold.co/300x200.png" },
-        { label: 'Nike Realmark', price: 10.1, items: 68, ava: "https://placehold.co/48x48", image: "/images/nike.png" },
-        { label: 'Nike Realmark', price: 10.1, items: 68, ava: "/images/nike.png", image: "/images/nike.png" },
-    ];
+    const { address } = useAccount()
+
+    const {
+        refetch: refetchGetAllCollection,
+        data: dataTotalCollectionsData,
+        isLoading: isLoadingTotalCollectionsData,
+        isError: isErrorTotalCollectionsData,
+        error: errorTotalCollectionsData
+    } = useReadContract({
+        address: COLLECTION_MANAGER_ADDRESS,
+        abi: COLLECTION_MANAGER_ABI,
+        functionName: 'getCollectionsByCreator',
+        args: [address]
+    })
+
+    const totalCollections = dataTotalCollectionsData ? Number(dataTotalCollectionsData) : 0;
+
+
+    const { data: collectionsData = [], isLoading: isLoadingCollections, isError: isErrorCollections, error: errorCollections } = useReadContracts({
+        contracts: Array.from({ length: totalCollections }, (_, i) => ({
+            address: COLLECTION_MANAGER_ADDRESS,
+            abi: COLLECTION_MANAGER_ABI,
+            functionName: 'getCollection',
+            args: [BigInt(i)]
+        })),
+        query: {
+            enabled: totalCollections > 0
+        },
+    });
 
     const handleCancelConfirmModalCloseEditModal = () => setModalEditCollection(false);
     const handleCancelActionEditModal = () => {
@@ -48,11 +80,18 @@ export default function Collections() {
     const handleChangeDataCollectionModal = (prop: any) => (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         setDataCollectionModal({ ...dataCollectionModal, [prop]: event.target.value })
     }
+
+    // useEffect(() => {
+    //     if (!isErrorGetAllCollection && !isErrorGetAllCollection && !isLoadingGetAllCollection && dataGetAllCollection) {
+    //         // ntr masukin sini 
+    //     }
+    // }, [dataGetAllCollection])
+
     return (
         <div id="collection-page-container" className="h-full">
-            <div id="collection-page-subtitle" className="text-Color-White-2/70 mt-2 mb-4 text-base font-semibold font-['D-DIN-PRO'] uppercase leading-none tracking-wide">Created collections ({collectionsData.length})</div>
+            <div id="collection-page-subtitle" className="h-full text-Color-White-2/70 mt-2 mb-4 text-base font-semibold font-['D-DIN-PRO'] uppercase leading-none tracking-wide">Created collections ({collectionsData.length})</div>
             {
-                collectionsData.length > 0 && (
+                collectionsData?.length > 0 && (
                     <div id="collection-card-wrapper" className="grid grid-cols-4 w-full gap-8">
                         {collectionsData.length > 0 && collectionsData.map((collection, key) => (
                             <CollectionCard linkHref="/collections/items" onEditClick={() => setModalEditCollection(true)} data={collection} />
